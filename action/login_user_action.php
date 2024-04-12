fea58f7b1aa84cc5700c726762c5a9a760e1af81
Arnold769 <138668623+Arnold769@users.noreply.github.com>
2024-04-12 19:33:53 +0000

    Update login_user_action.php

--- action/login_user_action.php
+++ action/login_user_action.php
@@ -3,7 +3,7 @@ session_start();
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-include './final/connections.php';
+include 'connections.php';
 
 if (isset($_POST['signIn'])) {
     $email = $_POST['email'];

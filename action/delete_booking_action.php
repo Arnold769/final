104d10d2aea0cc8191c0b195fa2891385a6638d5
Arnold769 <138668623+Arnold769@users.noreply.github.com>
2024-04-12 19:47:48 +0000

    Update delete_booking_action.php

--- action/delete_booking_action.php
+++ action/delete_booking_action.php
@@ -4,9 +4,9 @@ if (!isset($_SESSION)) {
     session_start();
 }
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+// ini_set('display_errors', 1);
+// ini_set('display_startup_errors', 1);
+// error_reporting(E_ALL);
 //include './core.php';
 $user_id = $_SESSION['user_id'];
 

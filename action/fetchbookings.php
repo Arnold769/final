aaa680d496932431df8dc6669c16daaef4b2b96c
Arnold769 <138668623+Arnold769@users.noreply.github.com>
2024-04-12 19:48:21 +0000

    Update fetchbookings.php

--- action/fetchbookings.php
+++ action/fetchbookings.php
@@ -1,9 +1,9 @@
 <?php
 include ('connections.php');
 session_start();
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+// ini_set('display_errors', 1);
+// ini_set('display_startup_errors', 1);
+// error_reporting(E_ALL);
 //include './core.php';
 
 function fetchBookings($userId)
